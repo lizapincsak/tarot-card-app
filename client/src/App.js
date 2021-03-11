0dd98077fd15994ec66237b1418791058662a1dd
Liza Pincsak <lizapincsak@MacBook.local>
2021-03-11 16:18:50 -0600

    wip

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import {useState, useEffect} from 'react';
+// import TarotCards from './TarotCards';
+// import data from './data';
+
+function getURL(path){
+    if(process.env.NODE_ENV === 'development'){
+        return 'http://localhost:5000' + path
+    } else {
+        return path
+    }
+}
+
+function App(){
+    const [tarot, setTarot] = useState("tarot")
+
+    useEffect(() => {
+        fetch(getURL('/api/tarot'))
+            .then(res => res.json())
+            .then(resBody => setTarot(resBody.tarot))
+            .catch(err => {
+                debugger
+            })
+    }, [])
+    return(
+        <div className="App">
+            <header>
+            <h1>Tarot Time</h1>
+            {tarot}
+            </header>
+        </div>
+    )
+}
+
+
+export default App;
